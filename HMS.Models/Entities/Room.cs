388a66c62024d4d32a4b8adc865797a472a0158d
lado koptonashvili <ladokop@gmail.com>
2025-04-08 20:33:50 +0400

    Add room_id property.

--- HMS.Models/Entities/Room.cs
+++ HMS.Models/Entities/Room.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace HMS.Models.Entities
     public class Room
     {
         [Key]
+        [Column("room_id")]
         public int Id { get; set; }
 
         [Required]
